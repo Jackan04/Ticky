6595fef5dbdb2da55f5d103056fa278b5256680f
Jackan04 <jacob.asker@icloud.com>
2025-10-24 17:11:19 +0200

    Improvment: Added handleClick function in ListPicker  for list selection and modal closure

--- src/components/Modal/ListPickerContent.jsx
+++ src/components/Modal/ListPickerContent.jsx
@@ -1,9 +1,16 @@
 import styles from "./Modal.module.css";
 import ListIcon from "../../assets/icons/list.svg?react";
 import { useList } from "../../context/listProvider.jsx";
+import { useModal } from "../../context/modalProvider.jsx";
 
 export default function ListPicker() {
   const { toggleActiveList, lists } = useList();
+  const { close } = useModal();
+
+  function handleClick(list) {
+    toggleActiveList(list);
+    close();
+  }
 
   return (
     <ul className={styles.listPicker}>
@@ -11,7 +18,7 @@ export default function ListPicker() {
         <li
           className={styles.list}
           key={list.id}
-          onClick={() => toggleActiveList(list)}
+          onClick={() => handleClick(list)}
         >
           {list.name}
           <ListIcon className={`icon ${styles.iconList}`} />
